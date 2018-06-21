f5fc8734edb5e7d15675c04789174ed3d9366f0d
Rohit K <rohitkalva@gmail.com>
2018-06-21 20:13:29 +0200

    Added registration post API and query

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(morgan('combined'))
 //DB Connection Configuration
 const  connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
-    password : 'Komrc@94',
-    database: 'test_nodejs'
+    user     : 'rfid',
+    password : 'rfidproject',
+    database: 'rfid'
 })
 
 //Connect to DB
@@ -35,6 +35,23 @@ app.get("/", (req, res) => {
     res.send("Hello from ROOOOT")
 })
 
+    app.get('/users1', (req, res) => {
+        //console.log("Fetching user with id: " + req.params.id)
+
+    const queryString = "SELECT * FROM db"
+
+    connection.query(queryString, (err, rows, fields) => {
+
+        if(err){
+            console.log("Failed to query " + err)
+            res.sendStatus(500)
+            return
+        }
+        console.log("Fetch Succesful")
+        res.json(rows)
+    })
+    })
+
 //Retrieve route
 app.get('/users', (req, res) => {
     //console.log("Fetching user with id: " + req.params.id)
@@ -52,7 +69,20 @@ connection.query(queryString, (err, rows, fields) => {
     res.json(rows)
 })
 })
+app.post('/registration', function(req, res) {
 
+    var jsondata = req.body;
+    var values = [];
+    
+    for(var i=0; i< jsondata.length; i++)
+      values.push([jsondata[i].tagID,jsondata[i].product, jsondata[i].purchasedate, jsondata[i].invoicenumber, jsondata[i].nextinspdate])
+    
+    //Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
+    connection.query('INSERT INTO registration (tagID, product, purchasedate, invoicenumber, nextinspdate) VALUES ?', [values], function(err,result) {
+        if (err) throw err;
+        res.end(JSON.stringify(result))
+    })
+    })
 
 app.post('/users', function(req, res) {
 
@@ -69,6 +99,6 @@ app.post('/users', function(req, res) {
     })
     })
 
-app.listen(3000, () => {
+app.listen(1080, () => {
     console.log("Server is up and listerning on port 3000")
 })
--- app_demo.js
+++ app_demo.js
@@ -1,76 +1,76 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const mysql = require('mysql')
-const Client = require('ssh2').Client;
-const ssh = new Client();
+    const express = require('express')
+    const app = express()
+    const morgan = require('morgan')
+    const bodyParser = require('body-parser')
+    const mysql = require('mysql')
+    const Client = require('ssh2').Client;
+    const ssh = new Client();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }))
 
-app.use(morgan('combined'))
+    app.use(morgan('combined'))
 
-const db = new Promise(function(resolve, reject){
-	ssh.on('ready', function() {
-	  ssh.forwardOut(
-	    // source address, this can usually be any valid address
-	    'localhost',
-	    // source port, this can be any valid port number
-	    3333,
-	    // destination address (localhost here refers to the SSH server)
-	    '165.227.137.107',
-	    // destination port
-	    22,
-	    function (err, stream) {
-	      if (err) throw err; // SSH error: can also send error in promise ex. reject(err)
-	      // use `sql` connection as usual
-	      	 connection = mysql.createConnection({
-	          host     : '127.0.0.1',
-	          user     : 'root',
-              password : 'Komrc@94', 
-              database: 'mysql',
-	          stream: stream
-	        });
+    const db = new Promise(function(resolve, reject){
+        ssh.on('ready', function() {
+        ssh.forwardOut(
+            // source address, this can usually be any valid address
+            '165.227.155.203',
+            // source port, this can be any valid port number
+            3333,
+            // destination address (localhost here refers to the SSH server)
+            '165.227.155.203',
+            // destination port
+            22,
+            function (err, stream) {
+            if (err) throw err; // SSH error: can also send error in promise ex. reject(err)
+            // use `sql` connection as usual
+                connection = mysql.createConnection({
+                host     : 'localhost',
+                user     : 'root',
+                password : 'rfidproject', 
+                database: 'mysql',
+                stream: stream
+                });
 
-	        // send connection back in variable depending on success or not
-		connection.connect(function(err){
-			if (!err) {
-				resolve(connection)
-			} else {
-				reject(err)
-			}
-		});
-	  });
-	}).connect({
-	  host: '165.227.137.107', //IP address where DB is hosted
-	  port: 22,               //Port refering to the IP 
-	  username: 'root',       //username to loginto the host  
-	  password: 'rohitkalva'  //password to log into host
+                // send connection back in variable depending on success or not
+            connection.connect(function(err){
+                if (!err) {
+                    resolve(connection)
+                } else {
+                    reject(err)
+                }
+            });
+        });
+        }).connect({
+        host: '165.227.155.203', //IP address where DB is hosted
+        port: 22,               //Port refering to the IP 
+        username: 'root',       //username to loginto the host  
+        password: 'rfidproject'  //password to log into host
+        });
     });
-});
 
-//Retrieve route
-app.get('/users', (req, res) => {
-    //console.log("Fetching user with id: " + req.params.id)
+    //Retrieve route
+    app.get('/users', (req, res) => {
+        //console.log("Fetching user with id: " + req.params.id)
 
-const queryString = "SELECT * FROM user"
+    const queryString = "SELECT * FROM sys_config"
 
-connection.query(queryString, (err, rows, fields) => {
+    connection.query(queryString, (err, rows, fields) => {
 
-    if(err){
-        console.log("Failed to query " + err)
-        res.sendStatus(500)
-        return
-    }
-    console.log("Fetch Succesful")
-    res.json(rows)
-})
-})
+        if(err){
+            console.log("Failed to query " + err)
+            res.sendStatus(500)
+            return
+        }
+        console.log("Fetch Succesful")
+        res.json(rows)
+    })
+    })
 
 
-app.listen(3000, () => {
-    console.log("Server is up and listerning on port 3000")
-})
+    app.listen(3000, () => {
+        console.log("Server is up and listerning on port 3000")
+    })
