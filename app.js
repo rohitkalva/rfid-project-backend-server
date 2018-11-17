603fba520aa8bb6e30394a66c2b471c7ad05a7f6
Rohit K <rohitkalva@gmail.com>
2018-11-17 14:17:41 +0100

    Moved api codes to separate controller files

--- app.js
+++ app.js
@@ -20,9 +20,12 @@ app.use(function(req, res, next) {
 
   var registerController=require('./controllers/register-controller');
   var authenticateController=require('./controllers/authenticate-controller');
+  var dataController=require('./controllers/data-controller')
 
   app.post('/api/register',registerController.register);
   app.post('/api/authenticate',authenticateController.authenticate);
+  app.post('/api/registration',dataController.registration);
+  app.post('/api/app1', dataController.app1);
 
 
 //Default Route
@@ -31,103 +34,22 @@ app.get("/", (req, res) => {
     res.send("Hello from ROOOOT")
 })
 
-//API to add registration details to DB
-app.post('/reg', function(req, res) {
+    // app.get('/users', (req, res) => {
+    //     //console.log("Fetching user with id: " + req.params.id)
 
-    console.log(req.body); 
-    // var jsondata = req.body;
-    // var values = [];
-    // for(var i=0; i< jsondata.length; i++)
-    //   values.push([jsondata[i].tagid,jsondata[i].equipment, jsondata[i].orderdate, jsondata[i].equipment_type, jsondata[i].labelling])
-    const tagid = req.body.tagid;
-    const equipment = req.body.equipment;
-    const orderdate = req.body.orderdate;
-    const equipment_type = req.body.equipment_type;
-    const labelling = req.body.labelling;
-    var queryString ='INSERT INTO reg (tagid, equipment, orderdate, equipment_type, labelling) VALUES (?, ?, ?, ?, ?)'
+    // const queryString = "SELECT * FROM reg"
 
-    // connection.query(queryString, [tagid, equipment, orderdate, equipment_type, labelling, tagid, orderdate], function(err,result) {
-    //     if (err) throw err;
-    //     return res.send({ error: false, data: result, message: 'Entry Successful!' });    })
+    // connection.query(queryString, (err, rows, fields) => {
 
-        //var query1 = "INSERT INTO dates (tagid, nextinspdate) VALUES (?, ?)"
-    // connection.query(query1, [tagid, orderdate], function(err,result){
-    //     if(err) throw err;
-    //     return res.send({error: false, data: result, message: 'Entry Successful!'});
-    // }) 
-
-    connection.beginTransaction(function(err) {
-        if (err) { throw err; }
-        connection.query(queryString, [tagid, equipment, orderdate, equipment_type, labelling, tagid, orderdate], function(err,result) {
-          if (err) { 
-            connection.rollback(function() {
-              throw err;
-            });
-          }
-       
-          var query1 = "INSERT INTO dates (tagid, nextinspdate) VALUES (?, ?)"
-       
-          connection.query(query1, [tagid, orderdate], function(err,result) {
-            if (err) { 
-              connection.rollback(function() {
-                throw err;
-              });
-            }  
-            connection.commit(function(err) {
-              if (err) { 
-                connection.rollback(function() {
-                  throw err;
-                });
-              }
-              console.log('Transaction Complete.');
-              return res.send({ error: false, data: result, message: 'Entry Successful!' });
-              connection.end();
-            });
-          });
-        });
-      });
-
-    })
-
-  //API to fetch TagIDs for mobile app
-  app.post('/app1', function(req, res){
-    const tagid = req.body
-    console.log(tagid)
-
-    //["11a4b3c243", "11a4b3c245", "11a4b3c247"] JSON Input for the API
-
-    const queryString = "select r.tagid, r.equipment, d.nextinspdate, r.equipment_type, r.labelling from reg r JOIN dates d WHERE r.tagid = d.tagid AND r.tagid IN (?)"
-
-    connection.query(queryString, [tagid], (err, result, fields) => {
-      
-      if(err){
-      console.log("Failed to query " + err)
-      res.sendStatus(500)
-       return
-      }
-      console.log("Fetch Succesful")
-      //res.json(rows)
-
-      return res.send({ error: false, data: result, message: 'Entry Successful!' });
-      })
-    })
-
-    app.get('/users', (req, res) => {
-        //console.log("Fetching user with id: " + req.params.id)
-
-    const queryString = "SELECT * FROM reg"
-
-    connection.query(queryString, (err, rows, fields) => {
-
-        if(err){
-            console.log("Failed to query " + err)
-            res.sendStatus(500)
-            return
-        }
-        console.log("Fetch Succesful")
-        res.json(rows)
-    })
-    })
+    //     if(err){
+    //         console.log("Failed to query " + err)
+    //         res.sendStatus(500)
+    //         return
+    //     }
+    //     console.log("Fetch Succesful")
+    //     res.json(rows)
+    // })
+    // })
 
 app.listen(1080, () => {
     console.log("Server is up and listerning on port 1080")
