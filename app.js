a17a1eb1359dc3f64cf2cb656958b268863694d0
Rohit K <rohitkalva@gmail.com>
2018-11-25 10:16:32 +0100

    Document formating

--- app.js
+++ app.js
@@ -12,22 +12,22 @@ app.use(bodyParser.urlencoded({
 
 app.use(morgan('short'))
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
-  var registerController=require('./controllers/register-controller');
-  var authenticateController=require('./controllers/authenticate-controller');
-  var dataController=require('./controllers/data-controller')
+var registerController = require('./controllers/register-controller');
+var authenticateController = require('./controllers/authenticate-controller');
+var dataController = require('./controllers/data-controller')
 
-  app.post('/api/register',registerController.register); //user registration
-  app.post('/api/authenticate',authenticateController.authenticate); //user authentication
-  app.post('/api/app/gettagdata', dataController.gettagdata); //get tag data in the app
-  app.post('/api/registration',dataController.registration); //new equipment registration
-  app.post('/api/app/updatetagdata', dataController.updatetagdata); //update tag data post inspection
-  app.post('/api/getreport', dataController.getreport); //fetch report between date ranges
+app.post('/api/register', registerController.register); //user registration
+app.post('/api/authenticate', authenticateController.authenticate); //user authentication
+app.post('/api/app/gettagdata', dataController.gettagdata); //get tag data in the app
+app.post('/api/registration', dataController.registration); //new equipment registration
+app.post('/api/app/updatetagdata', dataController.updatetagdata); //update tag data post inspection
+app.post('/api/getreport', dataController.getreport); //fetch report between date ranges
 
 
 
@@ -37,22 +37,22 @@ app.get("/", (req, res) => {
     res.send("Hello from ROOOOT")
 })
 
-    // app.get('/users', (req, res) => {
-    //     //console.log("Fetching user with id: " + req.params.id)
+// app.get('/users', (req, res) => {
+//     //console.log("Fetching user with id: " + req.params.id)
 
-    // const queryString = "SELECT * FROM reg"
+// const queryString = "SELECT * FROM reg"
 
-    // connection.query(queryString, (err, rows, fields) => {
+// connection.query(queryString, (err, rows, fields) => {
 
-    //     if(err){
-    //         console.log("Failed to query " + err)
-    //         res.sendStatus(500)
-    //         return
-    //     }
-    //     console.log("Fetch Succesful")
-    //     res.json(rows)
-    // })
-    // })
+//     if(err){
+//         console.log("Failed to query " + err)
+//         res.sendStatus(500)
+//         return
+//     }
+//     console.log("Fetch Succesful")
+//     res.json(rows)
+// })
+// })
 
 app.listen(1080, () => {
     console.log("Server is up and listerning on port 1080")
--- controllers/authenticate-controller.js
+++ controllers/authenticate-controller.js
@@ -1,46 +1,45 @@
 var Cryptr = require('cryptr');
 cryptr = new Cryptr('myTotalySecretKey');
- 
+
 var connection = require('./../config');
-module.exports.authenticate=function(req,res){
-    var username=req.body.username;
-    var password=req.body.password;
-  
-//Input format 
-//{
-//	"username": "username",
-//	"password": "password"
-//}
-   
-    connection.query('SELECT * FROM users WHERE username  = ?',[username], function (error, results, fields) {
-      if (error) {
-          res.json({
-            status:false,
-            message:'there are some error with query'
+module.exports.authenticate = function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    //Input format 
+    //{
+    //	"username": "username",
+    //	"password": "password"
+    //}
+
+    connection.query('SELECT * FROM users WHERE username  = ?', [username], function (error, results, fields) {
+        if (error) {
+            res.json({
+                status: false,
+                message: 'there are some error with query'
             })
-      }else{
-       
-        if(results.length >0){
-  decryptedString = cryptr.decrypt(results[0].password);
-            if(password==decryptedString){
+        } else {
+
+            if (results.length > 0) {
+                decryptedString = cryptr.decrypt(results[0].password);
+                if (password == decryptedString) {
+                    res.json({
+                        status: true,
+                        message: 'successfully authenticated'
+                    })
+                } else {
+                    res.json({
+                        status: false,
+                        message: "Username and password does not match"
+                    });
+                }
+
+            } else {
                 res.json({
-                    status:true,
-                    message:'successfully authenticated'
-                })
-            }else{
-                res.json({
-                  status:false,
-                  message:"Username and password does not match"
-                 });
+                    status: false,
+                    message: "User does not exit"
+                });
             }
-          
         }
-        else{
-          res.json({
-              status:false,    
-            message:"User does not exit"
-          });
-        }
-      }
     });
 }
