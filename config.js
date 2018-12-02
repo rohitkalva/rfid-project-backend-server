2e185a4fcf5d2233f07799d8bf9b0a8395de63c2
Rohit K <rohitkalva@gmail.com>
2018-12-03 00:49:54 +0100

    Added SSH connection layer to edit files locally. Backup of previous connection created in config_bak.js

--- config.js
+++ config.js
@@ -1,18 +1,45 @@
-var mysql = require('mysql');
+var mysql = require('mysql2');
+var Client = require('ssh2').Client;
+var ssh = new Client();
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'rfid',
-    password: 'rfidproject',
-    database: 'rfid',
-    dateStrings: true
-})
+var connection = new Promise(function(resolve, reject){
+	ssh.on('ready', function() {
+	  ssh.forwardOut(
+	    // source address, this can usually be any valid address
+	    '127.0.0.1',
+	    // source port, this can be any valid port number
+	    12345,
+	    // destination address (localhost here refers to the SSH server)
+	    '127.0.0.1',
+	    // destination port
+	    3306,
+	    function (err, stream) {
+	      if (err) throw err; // SSH error: can also send error in promise ex. reject(err)
+	      // use `sql` connection as usual
+	        var sql = mysql.createConnection({
+            host: 'localhost',
+            user: 'rfid',
+            password: 'rfidproject',
+            database: 'rfid',
+            dateStrings: true,
+	        stream: stream
+	        });
 
-connection.connect(function (err) {
-    if (!err) {
-        console.log("Database is connected");
-    } else {
-        console.log("Error while connecting with database");
-    }
+	        // send connection back in variable depending on success or not
+		sql.connect(function(err){
+			if (err) {
+				resolve(sql);
+			} else {
+				reject(err);
+			}
+		});
+	  });
+	}).connect({
+	  host: '46.101.232.21',
+	  port: 22,
+	  username: 'root',
+	  password: 'rfidproject'
+	});
 });
-module.exports = connection; //exporting connection
+
+module.exports = connection;
