16ba1ac7497542d8693610eb18b27d37ff1669de
Rohit K <rohitkalva@gmail.com>
2018-12-03 01:30:43 +0100

    Rollback changes done to cofig.js. Connection directly established on server

--- config.js
+++ config.js
@@ -1,45 +1,19 @@
-var mysql = require('mysql2');
-var Client = require('ssh2').Client;
-var ssh = new Client();
+var mysql = require('mysql');
 
-var connection = new Promise(function(resolve, reject){
-	ssh.on('ready', function() {
-	  ssh.forwardOut(
-	    // source address, this can usually be any valid address
-	    '127.0.0.1',
-	    // source port, this can be any valid port number
-	    12345,
-	    // destination address (localhost here refers to the SSH server)
-	    '127.0.0.1',
-	    // destination port
-	    3306,
-	    function (err, stream) {
-	      if (err) throw err; // SSH error: can also send error in promise ex. reject(err)
-	      // use `sql` connection as usual
-	        var sql = mysql.createConnection({
-            host: 'localhost',
-            user: 'rfid',
-            password: 'rfidproject',
-            database: 'rfid',
-            dateStrings: true,
-	        stream: stream
-	        });
+var connection = mysql.createConnection({
+		host: '46.101.232.21',
+		port: 3306,
+    user: 'rfid',
+    password: 'Rfid@123',
+    database: 'rfid',
+    dateStrings: true
+})
 
-	        // send connection back in variable depending on success or not
-		sql.connect(function(err){
-			if (err) {
-				resolve(sql);
-			} else {
-				reject(err);
-			}
-		});
-	  });
-	}).connect({
-	  host: '46.101.232.21',
-	  port: 22,
-	  username: 'root',
-	  password: 'rfidproject'
-	});
+connection.connect(function (err) {
+    if (!err) {
+        console.log("Database is connected");
+    } else {
+        console.log("Error while connecting with database");
+    }
 });
-
-module.exports = connection;
+module.exports = connection; //exporting connection
