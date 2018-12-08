c48ed133dca8883a4cc8fbf0818452c43eeb2402
Rohit <rohitkalva@gmail.com>
2018-12-08 18:12:28 +0100

    Added Uni Server configuration for database

--- config.js
+++ config.js
@@ -1,13 +1,23 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-		host: '46.101.232.21',
+		host: '141.44.18.16',
 		port: 3306,
-    user: 'rfid',
-    password: 'Rfid@123',
-    database: 'rfid',
-    dateStrings: true
+        user: 'inka',
+        password: '#inkaOvgu2018',
+        database: 'rfid',
+        dateStrings: true
 })
+
+// Old Server access
+// var connection = mysql.createConnection({
+//     host: '46.101.232.21',
+//     port: 3306,
+//     user: 'rfid',
+//     password: 'Rfid@123',
+//     database: 'rfid',
+//     dateStrings: true
+// })
 
 connection.connect(function (err) {
     if (!err) {
