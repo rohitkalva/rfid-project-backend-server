b29a0f015554f9f8776539f1c3e35ead72291915
Rohit K <rohitkalva@gmail.com>
2019-01-02 23:51:59 +0100

    New endpoint to update user password

--- controllers/authenticate-controller.js
+++ controllers/authenticate-controller.js
@@ -43,3 +43,44 @@ module.exports.authenticate = function (req, res) {
         }
     });
 }
+
+module.exports.passchange = function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var newpass = cryptr.encrypt(req.body.newpassword);
+
+    //Input format 
+    //{
+    //	"username": "username",
+    //	"password": "password"
+    //  "newpassword": "newpassword"
+    //}
+
+    connection.query('SELECT * FROM users WHERE username  = ?', [username], function (error, results, fields) {
+        if (error) {
+            res.json({
+                status: false,
+                message: 'Connection error'
+            })
+        } else {
+
+            if (results.length > 0) {
+                decryptedString = cryptr.decrypt(results[0].password);
+                if (password == decryptedString) {
+                    connection.query('UPDATE users SET password = ? WHERE username =? ',[newpass, username], function(error,results,fields){
+                        return res.json({
+                            status: true,
+                            message: "Update successful"
+                        })
+                    })
+                } else {
+                    res.json({
+                        status: false,
+                        message: "Wrong password"
+                    });
+                }
+
+            } 
+        }
+    });
+}
